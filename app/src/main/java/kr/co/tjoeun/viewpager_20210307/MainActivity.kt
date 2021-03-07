6497da1849b7cd7719d71cc5c8d75e9f9b865d3c
dennis <travlInfo4you@gmail.com>
2021-03-07 13:59:00 +0900

    페이지별 제목 문구 설정

--- app/src/main/java/kr/co/tjoeun/viewpager_20210307/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/viewpager_20210307/MainActivity.kt
@@ -26,6 +26,9 @@ class MainActivity : BaseActivity() {
         mvpa = MainViewPagerAdapter(supportFragmentManager)
 
         mainViewPager.adapter = mvpa
+
+        // xml에 배치한 탭레이아웃과 뷰페이저 연결
+         tabLayout.setupWithViewPager(mainViewPager)
     }
 
 
--- app/src/main/java/kr/co/tjoeun/viewpager_20210307/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/viewpager_20210307/adapters/MainViewPagerAdapter.kt
@@ -9,6 +9,13 @@ import kr.co.tjoeun.viewpager_20210307.fragments.NickFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position) {
+            0 -> "닉네임"
+            1 -> "생년월일"
+            else -> "소개문구"
+        }
+    }
     override fun getCount(): Int {
         return 3
 
@@ -16,7 +23,7 @@ class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
 /*
- 아래 return 문과 동일한 표현        
+ 아래 return 문과 동일한 표현
         if (position == 0) {
             return NickFragment()
         }
